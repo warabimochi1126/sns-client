7ade03def3e132118086e2c8b2898725b5c543fa
warabimochi <ecccompsenyou@gmail.com>
2023-11-28 13:26:42 +0900

    first commit

---
+++ src/lib/apiClient.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+  baseURL: "http://localhost:5000/api",
+  headers: {
+    "Content-Type": "application/json",
+    // "Authorization": `Bearer {token}`
+  },
+})
+
+export default apiClient;
+
+// axios.create でエンドポイントのURLやヘッダーを作成して使いまわせます
